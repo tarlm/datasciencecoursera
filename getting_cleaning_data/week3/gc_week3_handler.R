ea0282bb23c81e244852cd27414ecb040aa6be4b
radouhou <radouhou@free.fr>
2016-07-27 16:00:52 +0200

    add creating sequences part

--- getting_cleaning_data/week3/gc_week3_handler.R
+++ getting_cleaning_data/week3/gc_week3_handler.R
@@ -72,3 +72,11 @@ restData[restData$zipCode %in% c("21212", "21213"),]
 s1 <- seq(1, 10, by=2); s1
 s2 <- seq(1, 10, length=3); s2
 x <- c(1,3,8,25,100); seq(along = x)
+
+#subsetting variables
+restData$nearMe <- restData$neighborhood %in% c("Roland Park", "Homeland")
+table(restData$nearMe)
+
+# creating binary variables 
+restData$zipWrong = ifelse(restData$zipCode <0 , TRUE, FALSE)
+table(restData$zipWrong, restData$zipCode <0)
