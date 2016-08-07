9c985ea2d8230a0974c1e4c25edd1e4a54d5dcac
radouhou <radouhou@free.fr>
2016-08-07 23:44:35 +0200

    Step 1: merge datas completed

--- getting_cleaning_data/course_project/run_analysis.R
+++ getting_cleaning_data/course_project/run_analysis.R
@@ -7,8 +7,13 @@
 
 require(dplyr)
 
+# Step0 : Setup the working environnement and varibles 
+###############################################################################
 
 setwd("./getting_cleaning_data/course_project/") # first set my working directory
+
+# Clean up workspace
+rm(list=ls())
 
 # create data folder if not exist in order to store all ressources needed for the project
 if(!file.exists("./data")){
@@ -34,14 +39,52 @@ if (!file.exists(my_dataset_fld)) {
     unzip(zip_filename, exdir = data_fld_path)
 }
 
-# Load activity labels + features
+# Step1 : Merges the training and the test sets to create one data set. 
+###############################################################################
+
+# Load common data: activity labels + features
 activity_labels <- 
     read.table(file.path(my_dataset_fld,"activity_labels.txt"), stringsAsFactors = FALSE)
 
 features <- read.table(file.path(my_dataset_fld,"features.txt"), stringsAsFactors = FALSE)
 
-### 1. Merges the training and the test sets to create one data set. ###
-### 2. Extracts only the measurements on the mean and standard deviation for each measurement. ###
-### 3. Uses descriptive activity names to name the activities in the data set ###
-### 4. Appropriately labels the data set with descriptive variable names. ###
-### 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. ###
+# Load train data counts for 70% of observations: X_train.txt, y_train.txt, subject_train.txt
+x_train <- read.table(file.path(my_dataset_fld,"train/X_train.txt"), stringsAsFactors = FALSE)
+y_train <- read.table(file.path(my_dataset_fld,"train/y_train.txt"), stringsAsFactors = FALSE)
+subject_train <- read.table(file.path(my_dataset_fld,"train/subject_train.txt"), stringsAsFactors = FALSE)
+
+# Load text data counts for 30% of observations: X_test.txt, y_test.txt, subject_test.txt
+x_test <- read.table(file.path(my_dataset_fld,"test/X_test.txt"), stringsAsFactors = FALSE)
+y_test <- read.table(file.path(my_dataset_fld,"test/y_test.txt"), stringsAsFactors = FALSE)
+subject_test <- read.table(file.path(my_dataset_fld,"test/subject_test.txt"), stringsAsFactors = FALSE)
+
+#### Below are the requested data set ####
+
+# Merge 'x' datas(train and test) in order to 100% of 'X' observations
+x_full_data <- rbind(x_train, x_test)
+
+# Merge 'y' datas(train and test) in order to 100% of 'Y' observations
+y_full_data <- rbind(y_train, y_test)
+
+# Merge 'subject' datas(train and test) in order to 100% of 'subject' observations
+subject_full_data <- rbind(subject_train, subject_test)
+
+
+# Step2. Extracts only the measurements on the mean and standard deviation for each measurement.
+###############################################################################
+
+
+
+
+# Step3. Uses descriptive activity names to name the activities in the data set
+###############################################################################
+
+
+# Step4. Appropriately labels the data set with descriptive variable names.
+###############################################################################
+
+
+# Step5. From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+###############################################################################
+
