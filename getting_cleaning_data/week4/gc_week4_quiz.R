f423f424b59851e2f3fffc4ba04951def37dc291
radouhou <radouhou@free.fr>
2016-08-07 09:50:15 +0200

    update the quiz script for week 4 of getting and cleaning data

--- getting_cleaning_data/week4/gc_week4_quiz.R
+++ getting_cleaning_data/week4/gc_week4_quiz.R
@@ -6,7 +6,7 @@
 #################################################################################
 
 require(data.table)
-require(dplyr)
+require(quantmod)
 
 setwd("./getting_cleaning_data/week4/") # first set my working directory
 
@@ -40,56 +40,66 @@ fgdp_csv_file_path <- file.path(getwd(), "data/data_FGDP.csv") # set file_path o
 download.file(fgdp_file_url, fgdp_csv_file_path, mode = "wb" , method = "libcurl")
 
 # load the csv in order to explore the file structure
-fgdp_df <- read.csv(fgdp_csv_file_path, header = FALSE) 
-# explore data
+fgdp_df <- data.table(read.csv(fgdp_csv_file_path, skip = 4, nrows = 190,
+                               stringsAsFactors = FALSE))
+# View data
 View(fgdp_df)
 
-# load the csv files into data table with corrects filters
-fgdp_df <- fread(fgdp_csv_file_path, skip = 5, header = FALSE, nrows = 190, select = c(1, 2, 4, 5), 
-                 col.names = c("CountryCode", "ranking", "countryNames", "GDP"))
+fgdp_df <- fgdp_df[, list(X, X.1, X.3, X.4)]
+
+setnames(fgdp_df, c("X", "X.1", "X.3", "X.4"), 
+         c("CountryCode", "ranking", "countryNames", "GDP"))
 
 View(fgdp_df)
 
-fgdp_df <- mutate(fgdp_df, GDP = as.numeric(gsub(",", "", fgdp_df$GDP)))
+#fgdp_df <- mutate(fgdp_df, GDP = as.numeric(gsub(",", "", fgdp_df$GDP)))
 
-#my_gdp <- as.numeric(gsub(",", "", fgdp_df$GDP))
+gdp <- as.numeric(gsub(",", "", fgdp_df$GDP))
 
 #the resultat is [1] 377652.4
-mean(fgdp_df$GDP)
+mean(gdp, na.rm = TRUE)
 
 ##### Question 3:  ##### 
 
-grep("^United",fgdp_df$countryNames)
+united <- grepl("United",fgdp_df$countryNames)
+summary(united)
 
 #the result is grep("^United",countryNames), 3
+
 
 ##### Question 4:  ##### 
 
 # Load the educational data from this data set:
-fedu_file_url <- 
+fedStat_file_url <- 
     "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
 
-fedu_csv_file_path <- file.path(getwd(), "data/data_fedu.csv") # set file_path on system
+fedStat_csv_file_path <- file.path(getwd(), "data/data_fedStat.csv") # set file_path on system
 
-download.file(fedu_file_url, fedu_csv_file_path, mode = "wb" , method = "libcurl")
+download.file(fedStat_file_url, fedStat_csv_file_path, mode = "wb" , method = "libcurl")
 
-fedu_df <- read.csv(fedu_csv_file_path, header = TRUE) 
+fedStat_df <- data.table(read.csv(fedStat_csv_file_path, header = TRUE))
 
-View(fedu_df)
+View(fedStat_df)
 
-fedu_df_tb <- tbl_df(fedu_df)
+#fedStat_df_tb <- tbl_df(fedStat_df)
+names(fedStat_df)
 
-matched_table <- merge(fgdp_df, fedStat_df, by = 'CountryCode')
+matched_table <- merge(fgdp_df, fedStat_df, all = TRUE, by = 'CountryCode')
 
+isFiscalYearEnd <- grepl("fiscal year end", tolower(matched_table$Special.Notes))
 
+isJune <- grepl("june", tolower(matched_table$Special.Notes))
 
+table(isFiscalYearEnd, isJune)
+# the result is 13
+matched_table[isFiscalYearEnd & isJune, Special.Notes]
 
+##### Question 5:  ##### 
 
+amzn <- getSymbols("AMZN", auto.assign=FALSE)
+sampleTimes <- index(amzn)
 
+View(amzn)
 
-
-
-
-
-
-
+# The result is 2012    51    47    50       51       51  250
+addmargins(table(year(sampleTimes), weekdays(sampleTimes)))
